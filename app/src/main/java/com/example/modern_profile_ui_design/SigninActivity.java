399c498763c51b41369add19804498bb6c203c35
Phat Nguyen <vanphattk159@gmail.com>
2022-09-12 18:02:09 +0700

    first commit

---
+++ app/src/main/java/com/example/modern_profile_ui_design/SigninActivity.java
@@ -0,0 +1,11 @@
+package com.example.modern_profile_ui_design;
+
+import android.os.Bundle;
+
+public class SigninActivity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
---
+++ app/src/main/java/com/example/modern_profile_ui_design/SignupActivity.java
@@ -0,0 +1,11 @@
+package com.example.modern_profile_ui_design;
+
+import android.os.Bundle;
+
+public class SignupActivity extends MainActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
