7f4301840bbff2004c5c070a6b5ed30f3c68c1fa
Phat Nguyen <vanphattk159@gmail.com>
2022-09-13 19:53:46 +0700

    second commit

--- app/src/main/java/com/example/modern_profile_ui_design/SigninActivity.java
+++ app/src/main/java/com/example/modern_profile_ui_design/SigninActivity.java
@@ -1,11 +1,44 @@
 package com.example.modern_profile_ui_design;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 public class SigninActivity extends MainActivity {
+    private Button btnLogin,btnSignUp;
+    private TextView user, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        btnLogin = (Button) findViewById(R.id.signin);
+        btnSignUp = (Button) findViewById(R.id.signup);
+        user = (TextView) findViewById(R.id.username);
+        password = (TextView) findViewById(R.id.password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(user.getText().toString().equals("admin") && password.getText().toString().equals("12345"))
+                {
+                    Intent intent = new Intent(SigninActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/modern_profile_ui_design/SignupActivity.java
+++ app/src/main/java/com/example/modern_profile_ui_design/SignupActivity.java
@@ -1,11 +1,28 @@
 package com.example.modern_profile_ui_design;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 
 public class SignupActivity extends MainActivity{
+    private Button btnLogin;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        btnLogin = (Button) findViewById(R.id.signin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
